e2b486a152aaefbf251fbcad3189d3978df7023a
etwin <etwinhendri@gmail.com>
2023-12-14 06:33:13 +0000

    feat: Add dotenv configuration and token verification function
    Added dotenv configuration and a function to verify tokens

--- controller/controller.js
+++ controller/controller.js
@@ -1,11 +1,29 @@
-async function reqToken(req, res) {
+require("dotenv").config();
+// Mendapatkan SECRET_KEY yang diharapkan dari environment variable (env)
+const kode_env = process.env.SECRET_KEY;
+async function reqToken(req, res) {}
 
+function verify(token) {
+  let result = {};
+
+  jwt.verify(token, kode_env, (err, decoded) => {
+    if (err) {
+      result = {
+        status: false,
+      };
+    } else {
+      // cek db identity
+      result = {
+        nama_db: decoded,
+        status: true,
+      };
+    }
+  });
+
+  return result;
 }
-
-
 async function inputData(req, res) {
   const { token, volume, debit } = req.body;
-
 }
 
 async function dashboard(req, res) {}
